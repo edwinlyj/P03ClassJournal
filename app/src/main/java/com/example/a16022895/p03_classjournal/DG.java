968377fe8a0e7d79bc895e299858869e75a82e82
16023054 <16023054@myrp.edu.sg>
2018-05-07 14:40:51 +0800

    pushhhhhhhh

--- app/src/main/java/com/example/a16022895/p03_classjournal/DG.java
+++ app/src/main/java/com/example/a16022895/p03_classjournal/DG.java
@@ -2,12 +2,24 @@ package com.example.a16022895.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DG extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<DailyGrade> dailyGrades;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dg);
+
+        dailyGrades = new ArrayList<DailyGrade>();
+        lv = (ListView) this.findViewById(R.id.dg);
+
     }
 }

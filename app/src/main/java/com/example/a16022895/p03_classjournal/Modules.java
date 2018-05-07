f5b547ee279c8f8af869252da67e0c09fdd315ec
16022895 <16022895@myrp.edu.sg>
2018-05-07 12:39:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022895/p03_classjournal/Modules.java
@@ -0,0 +1,12 @@
+package com.example.a16022895.p03_classjournal;
+
+public class Modules {
+
+    private String mmoduleName;
+    private String mmoduleCode;
+
+    public Modules(String mmoduleName, String mmoduleCode) {
+        this.mmoduleName = mmoduleName;
+        this.mmoduleCode = mmoduleCode;
+    }
+}

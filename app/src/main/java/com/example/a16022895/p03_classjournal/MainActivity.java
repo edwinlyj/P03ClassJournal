8482b0d0ff520333e0a710b0c9dee55ac13511ed
16023054 <16023054@myrp.edu.sg>
2018-05-07 12:45:51 +0800

    listview in activity_main

--- app/src/main/java/com/example/a16022895/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/example/a16022895/p03_classjournal/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.a16022895.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.lv);
     }
 }

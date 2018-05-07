cba8c632c8a17e2f23452b5aaf8e54a359e99310
16033760 <16033760@myrp.edu.sg>
2018-05-07 14:44:20 +0800

    Info button and Email Button

--- app/src/main/java/com/example/a16022895/p03_classjournal/DG.java
+++ app/src/main/java/com/example/a16022895/p03_classjournal/DG.java
@@ -1,13 +1,46 @@
 package com.example.a16022895.p03_classjournal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DG extends AppCompatActivity {
+
+    Button btnRP;
+    Button btnEmails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dg);
+
+        btnRP = (Button) findViewById(R.id.buttonInfo);
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
+        btnEmails = (Button) findViewById(R.id.buttonEmail);
+        btnEmails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT,
+                        "Test Email from C347");
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
+            }
+        });
     }
 }
